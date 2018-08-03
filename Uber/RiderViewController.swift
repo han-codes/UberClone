0fd0cd98400de7e6742c49a8137a55a20cfca7e9
Han_Codes <hanniek55@gmail.com>
2018-08-03 10:16:48 -0500

    Request user for location authorization and add annotations to our map

--- Uber/RiderViewController.swift
+++ Uber/RiderViewController.swift
@@ -9,16 +9,45 @@
 import UIKit
 import MapKit
 
-class RiderViewController: UIViewController {
+class RiderViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var map: MKMapView!
     @IBOutlet weak var callAnUberButton: UIButton!
     
+    // need CLLocationManager() to get location
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Set delegate to self, best accuracy, request authorization when location is in use, start to update the location
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    // when location is updated
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // gets coordinate of the user. Can access longitude and latitude from t
+        if let coordinate = manager.location?.coordinate {
+            let center = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
+            
+            // define the region and how much space it should show using Span
+            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            
+            // update map with our region
+            map.setRegion(region, animated: true)
+            
+            // remove annotations before setting new ones, so the previous ones aren't saved
+            map.removeAnnotations(map.annotations)
+            
+            // Pin to represent the user's location
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = center
+            annotation.title = "Your Location"
+            map.addAnnotation(annotation)
+            
+        }
     }
 
     @IBAction func logoutTapped(_ sender: UIBarButtonItem) {
