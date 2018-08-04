65d95cb5a5ed28b82e1fb033861f9e896b67c8a6
Han_Codes <hanniek55@gmail.com>
2018-08-04 08:08:02 -0500

    Add users' values to the database. If user cancels uber, remove their data from database.

--- Podfile
+++ Podfile
@@ -8,4 +8,5 @@ target 'Uber' do
   # Pods for Uber
     pod 'Firebase/Core'
     pod 'Firebase/Auth'
+    pod 'Firebase/Database'
 end
--- Uber/RiderViewController.swift
+++ Uber/RiderViewController.swift
@@ -8,14 +8,22 @@
 
 import UIKit
 import MapKit
+import FirebaseDatabase
+import FirebaseAuth
 
 class RiderViewController: UIViewController, CLLocationManagerDelegate {
-
+    
     @IBOutlet weak var map: MKMapView!
     @IBOutlet weak var callAnUberButton: UIButton!
     
     // need CLLocationManager() to get location
     var locationManager = CLLocationManager()
+    
+    // used to set value of lon and lat for our database in callUberTpped
+    var userLocation = CLLocationCoordinate2D()
+    
+    // will keep track if user has clicked to call an uber or not
+    var uberHasBeenCalled = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +39,9 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate {
         // gets coordinate of the user. Can access longitude and latitude from t
         if let coordinate = manager.location?.coordinate {
             let center = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
+            
+            // used in callUberTapped method
+            userLocation = center
             
             // define the region and how much space it should show using Span
             let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
@@ -49,12 +60,44 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate {
             
         }
     }
-
+    
+    @IBAction func callUberTapped(_ sender: UIButton) {
+        
+        // gets the current user's email address
+        if let email = Auth.auth().currentUser?.email {
+            
+            if uberHasBeenCalled {
+                uberHasBeenCalled = false
+                callAnUberButton.setTitle("Call an Uber", for: .normal)
+                
+                // Remove the user's values since they cancelled their uber
+                Database.database().reference().child("RideRequests").queryOrdered(byChild: "email").queryEqual(toValue: email).observe(.childAdded) { (snapshot) in
+                    snapshot.ref.removeValue()
+                    Database.database().reference().child("RideRequests").removeAllObservers()
+                }
+            } else {
+                // dictionary will be passed into the RideRequests table
+                let rideRequestDictionary : [String:Any] = ["email":email, "lat":userLocation.latitude, "lon":userLocation.longitude]
+                
+                // the child RideRequests in our database, gets it's values set by let rideRequestDictionary
+                // childByAutoId() will provide the random unique id
+                Database.database().reference().child("RideRequests").childByAutoId().setValue(rideRequestDictionary)
+                
+                // set to true since they clicked on to call the uber
+                uberHasBeenCalled = true
+                
+                // change the text of the button to give user option to cancel the uber
+                callAnUberButton.setTitle("Cancel Uber", for: .normal)
+            }
+        }
+        
+        
+    }
+    
     @IBAction func logoutTapped(_ sender: UIBarButtonItem) {
         
     }
     
-    @IBAction func callUberTapped(_ sender: UIButton) {
-    }
+    
     
 }
