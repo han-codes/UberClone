7baa8db3c7572c98ccd83c1b983cc7e559040ed9
Han_Codes <hanniek55@gmail.com>
2018-08-06 15:13:55 -0500

    Get riders' emails from database and display them in the cell text labels. Set up location of driver

--- Uber/DriverTableViewController.swift
+++ Uber/DriverTableViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 import FirebaseAuth
 import FirebaseDatabase
+import MapKit
 
 class DriverTableViewController: UITableViewController {
 
+    // hold the ride requests in DataSnapshot type
+    // snapshot used in viewDidLoad is a DataSnapshot data type
+    var rideRequests : [DataSnapshot] = []
+    var locationManager = CLLocationManager()
+    var driverLocation = CLLocationCoordinate2D()   // stores the driver's location
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        Database.database().reference().child("RideRequests").observe(.childAdded) { (snapshot) in
+            // add the snapshot to our array that holds all of them
+            self.rideRequests.append(snapshot)
+            // refresh every time we add something new.
+            self.tableView.reloadData()
+        }
     }
    
     // MARK: - Table view data source
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
+        return rideRequests.count
     }
     
     @IBAction func logoutTapped(_ sender: UIBarButtonItem) {
@@ -31,7 +47,15 @@ class DriverTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "rideRequestCell", for: indexPath)
         
-        cell.textLabel?.text = "Hello"
+        // gets the snapshot for the specific row
+        let snapshot = rideRequests[indexPath.row]
+        
+        // gets the emails of the users in the database and displays the the cells
+        if let rideRequestDictionary = snapshot.value as? [String:AnyObject] {
+            if let email = rideRequestDictionary["email"] as? String {
+                    cell.textLabel?.text = email
+            }
+        }
         
         return cell
     }
